5eed05892b0dff967a605533d2a8a1b9fe13f06f
rik61072 <rik61072@gmail.com>
2017-08-31 23:55:37 +0530

    Plist related functions are done even though I have not tested them yet

--- function.py
+++ function.py
@@ -1,5 +1,35 @@
 import os
+import re
+import plistlib
 
-for root , dirs , files in os.walk("/"):
-	pass
-	
+__author__ = "Rik \"Rik\" Bhattacharya "
+__doc__ = '''
+Free and good uninstallers for macOS are not available, so I decided to make my own. This file serves as a library and stores some necessary functions. A combination of these functions will be used to code the uninstaller
+
+Open Source For Life
+Free stuff for life
+
+I have nothing else to say
+
+'''
+
+
+
+def read_plist(directory):
+	''' This function returns a tuple which contains (bundle_identifier , bundle_name , bundle_signature) of an app '''
+	plistfile = plistlib.readPlist(directory)
+	bundle_identifier = plistfile['CFBundleIdentifier']
+	bundle_signature = plistfile['CFBundleSignature']
+	bundle_name = plistfile['CFBundleName']
+	return (bundle_identifier , bundle_name , bundle_signature)    #not in order :P
+
+def find_plist(app_dir):
+	''' This function returns a string which points to the file location of Info.plist '''
+	return app_dir + "/Contents/Info.plist"
+
+
+#for root , dirs , files in os.walk("/"):
+	#pass
+
+
+
