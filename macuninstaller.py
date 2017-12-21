333cb0a5e8e8db9f86a9c599807e523bdfd2bc91
rik61072 <rik61072@gmail.com>
2017-12-21 18:56:38 +0530

    updated gui

--- macuninstaller.py
+++ macuninstaller.py
@@ -1,4 +1,6 @@
 from function import *
+import shutil
+import os
 
 app_dir = app_name_asker("Enter the path name")
 
@@ -7,8 +9,8 @@ files , folders = thread_scanner(app_dir)
 files = cleanup(files)
 folders = cleanup(folders)
 
-file_list = displayer(files)
-folder_list = displayer(folders)
+file_list = displayer(files, "Files found")
+folder_list = displayer(folders, "Folders found")
 
 files_to_delete = []
 folders_to_delete = []
@@ -21,6 +23,9 @@ for stuff in folder_list.items():
     if stuff[1]:
         folders_to_delete.append(stuff[0])
 
-printer(files_to_delete)
-print "\n"*2
-printer(folders_to_delete)
+
+for stuff in files_to_delete:
+    os.remove(stuff)
+
+for stuff in folders_to_delete:
+    shutil.rmtree(stuff)
