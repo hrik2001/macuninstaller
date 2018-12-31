727712bbc1142230bbb44936721871ceb29d7c39
Shatabarto Bhattacharya <rik61072@gmail.com>
2018-12-31 14:52:58 +0530

    Applescript function works now

--- macuninstaller.py
+++ macuninstaller.py
@@ -1,6 +1,15 @@
 from function import *
+import os
+
+if os.getuid() != 0:
+	cmd = '''display dialog "You need root privileges to run this script mate (HINT: run this python script with sudo)" with icon stop with title "Oops!"'''
+	cmd = "osascript -e '" + cmd + "'" 
+	a = os.popen(cmd)
+	a.read()
+	exit()
 
 if chooser():
     applescript_default_scanner()
 else:
     applescript_custom_scanner()
+
